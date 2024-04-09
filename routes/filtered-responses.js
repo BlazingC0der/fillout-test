66c236d9a0888360f999f595683ae9ec420f1633
ammar-roxova <152115452+ammar-roxova@users.noreply.github.com>
2024-04-09 16:43:51 +0500

    third commit

--- routes/filtered-responses.js
+++ routes/filtered-responses.js
@@ -4,3 +4,4 @@ import { getFilteredResponses } from "../controllers/filtered-responses.js"
 export const router = Router()
 
 router.get("/:formId/filteredResponses", getFilteredResponses)
+router.get("/",(req,res) => { res.sendStatus(200) })
